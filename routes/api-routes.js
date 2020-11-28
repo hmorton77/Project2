7161a9f57eecb3f2463da489d96dc3891469071a
hmorton77 <harold.m.morton@gmail.com>
2020-11-28 11:54:12 -0500

    added more api-route data

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,7 @@
+//dependency
 var db = require("../models");
 
+//routes
 module.exports = function (app) {
   // get all the cases to be put on the main page
   app.get("/api/cases", function (req, res) {
@@ -27,7 +29,7 @@ module.exports = function (app) {
       res.json(dbQuar);
     });
   });
-  //posting a new employee case
+  //posting a new employee case FROM THE SUBMISSION FORM
   app.post("/api/cases", function (req, res) {
     db.Quarantine.Create({
       firstName: req.body.firstName,
@@ -36,6 +38,51 @@ module.exports = function (app) {
       quarantineEnd: req.body.quarantineEnd,
       covidConfirmed: req.body.covidConfirmed,
       currentCondition: req.body.currentCondition,
+    }).then(function (dbQuar) {
+      res.json(dbQuar);
     });
+  });
+  //"delete" route will just change the active status to false
+  app.put("/api/cases", function (req, res) {
+    db.Quarantine.update(
+      {
+        activeStatus: false,
+      },
+      {
+        where: {
+          id: req.body.id,
+        },
+      }
+    )
+      .then(function (dbQuar) {
+        res.json(dbQuar);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
+  //update route to update posts
+  app.put("/api/cases", function (req, res) {
+    db.Quarantine.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        quarantineStart: req.body.quarantineStart,
+        quarantineEnd: req.body.quarantineEnd,
+        covidConfirmed: req.body.covidConfirmed,
+        currentCondition: req.body.currentCondition,
+      },
+      {
+        where: {
+          id: req.body.id,
+        },
+      }
+    )
+      .then(function (dbQuar) {
+        res.json(dbQuar);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
   });
 };
