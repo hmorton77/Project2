49f52a8d97a2af437b298b5ba591e1ebee0fb128
hmorton77 <harold.m.morton@gmail.com>
2020-12-02 19:27:13 -0500

    tinkering with handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,13 +1,13 @@
 var path = require("path");
-
+var potato = [potato];
 module.exports = function (app) {
   // our home page which we should get when we open the app
   app.get("/", function (req, res) {
-    res.render("index", Quarantine);
+    res.render("index", potato);
   });
   // the submission page when we open the "add patient page"
   app.get("/submit", function (req, res) {
-    res.render("submission", Quarantine);
+    res.render("submission", employee);
   });
   //open the symptomatic employee page
   app.get("/symptomatic", function (req, res) {
