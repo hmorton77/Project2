0cc28ec194b649b8a78d4c2e8ebdd92cf4ee7b3a
hmorton77 <harold.m.morton@gmail.com>
2020-12-02 19:04:23 -0500

    added navbar.handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,11 +3,11 @@ var path = require("path");
 module.exports = function (app) {
   // our home page which we should get when we open the app
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/..public/views/layouts/index.handlebars/"));
+    res.render("index", Quarantine);
   });
   // the submission page when we open the "add patient page"
   app.get("/submit", function (req, res) {
-    res.sendFile(path.join(__dirname, "..public/"));
+    res.render("submission", Quarantine);
   });
   //open the symptomatic employee page
   app.get("/symptomatic", function (req, res) {
