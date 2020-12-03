ee5918fd104b38ecb44c456a5c2d0afa54d6a117
Cyle <cmac2617@yahoo.com>
2020-12-02 20:54:02 -0500

    Updates to update.js

--- public/js/update.js
+++ public/js/update.js
@@ -1,3 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    function updateEmployee(data) {
+        $.post("/api/cases/", data)
+    };
+    var firstNameInput = $("#firstName");
+    var lastNameInput = $("#lastName");
+    var PosDate = $("#posDate");
+    var condition = $("#condition");
+    var qStart = $("#qStart");
+    var qEnd = $("#qEnd");
 
+    function updatePost(post) {
+        $.ajax({
+            method: "PUT",
+            url: "/api/posts",
+            data: post
+        })
+            .then(function () {
+                window.location.href = "/hospitalized.html";
+            });
+    }
+
+    $("#updateButton").on("click", function () {
+        var updatedInfo = {
+            firstName: firstNameInput.val().trim(),
+            lastName: lastNameInput.val().trim(),
+            covidConfirmed: PosDate.val().trim(),
+            currentCondition: condition.val().trim(),
+            quarantineStart: qStart.val().trim(),
+            quarantineEnd: qEnd.val().trim()
+        }
+        console.log(updatedInfo);
+        updatePost(updatedInfo);
+
+    })
 });
+
