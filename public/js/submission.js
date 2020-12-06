1feebbd65a82ada9ce7e77a7c09b8d2a644ea0f2
hmorton77 <harold.m.morton@gmail.com>
2020-12-06 09:53:38 -0500

    misc fixes

--- public/js/submission.js
+++ public/js/submission.js
@@ -1,80 +1,70 @@
 $(document).ready(function () {
-    // webshim.setOptions('forms-ext', {
-    //     replaceUI: 'auto',
-    //     types: 'date',
-    //     date: {
-    //         startView: 2,
-    //         inlinePicker: true,
-    //         classes: 'hide-inputbtns'
-    //     }
-    // });
-    // webshim.setOptions('forms', {
-    //     lazyCustomMessages: true
-    // });
-    // //start polyfilling
-    // webshim.polyfill('forms forms-ext');
+  // webshim.setOptions('forms-ext', {
+  //     replaceUI: 'auto',
+  //     types: 'date',
+  //     date: {
+  //         startView: 2,
+  //         inlinePicker: true,
+  //         classes: 'hide-inputbtns'
+  //     }
+  // });
+  // webshim.setOptions('forms', {
+  //     lazyCustomMessages: true
+  // });
+  // //start polyfilling
+  // webshim.polyfill('forms forms-ext');
 
-    // var routes = require("../routes/api-routes.js");
+  // var routes = require("../routes/api-routes.js");
 
-    //only last example using format display
-    $(function () {
-        $('.format-date').each(function () {
-            var $display = $('.date-display', this);
-            $(this).on('change', function (e) {
-                //webshim.format will automatically format date to according to webshim.activeLang or the browsers locale
-                var localizedDate = webshim.format.date($.prop(e.target, 'value'));
-                $display.html(localizedDate);
-            });
-        });
+  //only last example using format display
+  $(function () {
+    $(".format-date").each(function () {
+      var $display = $(".date-display", this);
+      $(this).on("change", function (e) {
+        //webshim.format will automatically format date to according to webshim.activeLang or the browsers locale
+        var localizedDate = webshim.format.date($.prop(e.target, "value"));
+        $display.html(localizedDate);
+      });
     });
+  });
 
-    var firstNameInput = $("#firstName");
-    var lastNameInput = $("#lastName");
-    var hosp = $("#hosp");
-    var condition = $("#condition");
-    var qStart = $("#qStart");
-    var qEnd = $("#qEnd");
+  var firstNameInput = $("#firstName");
+  var lastNameInput = $("#lastName");
+  var hosp = $("#hosp");
+  var condition = $("#condition");
+  var qStart = $("#qStart");
+  var qEnd = $("#qEnd");
 
-    function submitEmployee(data) {
-        $.post("/api/cases/", data)
+  function submitEmployee(data) {
+    $.post("/api/cases/", data);
+  }
+
+  // Click event to record employee details and call post request.
+  $("#newEmp").on("click", function () {
+    // event.preventDefault();
+    console.log("clicked");
+    // On success, post to the console.
+    var newEmployee = {
+      firstName: firstNameInput.val().trim(),
+      lastName: lastNameInput.val().trim(),
+      hospitalized: hosp.val().trim(),
+      currentCondition: condition.val().trim(),
+      quarantineStart: qStart.val().trim(),
+      quarantineEnd: qEnd.val().trim(),
     };
-    
-
-
-
-    // Click event to record employee details and call post request.
-    $("#newEmp").on("click", function () {
-        // event.preventDefault();
-        console.log("clicked");
-        // On success, post to the console.
-        var newEmployee = {
-            firstName: firstNameInput.val().trim(),
-            lastName: lastNameInput.val().trim(),
-            hospitalized: hosp.val().trim(),
-            currentCondition: condition.val().trim(),
-            quarantineStart: qStart.val().trim(),
-            quarantineEnd: qEnd.val().trim()
-        };
-        console.log(newEmployee);
-        submitEmployee(newEmployee);
-    });
-
-
-
-
+    console.log(newEmployee);
+    submitEmployee(newEmployee);
+  });
 });
 
-
 function test() {
-    alert("Information Submitted!");
-    console.log($('#validationCustom01').val())
-    console.log($('#validationCustom02').val())
-    console.log($('#validationCustom03').val())
-    console.log($('#validationCustom04').val())
-    console.log($('#validationCustom05').val())
-    console.log($('#validationCustom06').val())
-    return true;
- }
-
-
-
+  alert("Information Submitted!");
+  console.log($("#validationCustom01").val());
+  console.log($("#validationCustom02").val());
+  console.log($("#validationCustom03").val());
+  console.log($("#validationCustom04").val());
+  console.log($("#validationCustom05").val());
+  console.log($("#validationCustom06").val());
+  console.log($("#exampleInputEmail1").val());
+  return true;
+}
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,5 @@
 var path = require("path");
 var db = require("../models");
-var potato = ["this is some data about potatoes"];
 module.exports = function (app) {
   // our home page which we should get when we open the app
   app.get("/", function (req, res) {
@@ -50,7 +49,8 @@ module.exports = function (app) {
       res.render("hospitalized", employeeObj);
     });
   });
+  // open the reminder page
   app.get("/reminder", function (req, res) {
-    res.render("reminder", potato);
+    res.render("reminder");
   });
 };
