561fa80231d4ac9bd4fabb216d613156afc8e697
hmorton77 <harold.m.morton@gmail.com>
2020-12-07 16:50:44 -0500

    added app.set statement

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.set("port", PORT);
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
